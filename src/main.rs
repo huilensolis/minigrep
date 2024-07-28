02cff1d517a0e599926f5918f800f9ccc2ef7278
Huilensolis <xywvanz@gmail.com>
2024-07-28 20:16:17 -0300

    -- feat: send input through stdrerr instead of stdout --

--- src/main.rs
+++ src/main.rs
@@ -7,12 +7,12 @@ fn main() {
     let args: Vec<String> = env::args().collect();
 
     let config = Config::new(&args).unwrap_or_else(|error| {
-        println!("error parsing arguments: {}", error);
+        eprintln!("error parsing arguments: {}", error);
         process::exit(1)
     });
 
     minigrep::run(&config).unwrap_or_else(|error| {
-        println!("error reading file: {}", error);
+        eprintln!("error reading file: {}", error);
         process::exit(1)
     })
 }
